70bf6d8e43f19c12f7a29929ff22a1ae090ed90e
jkoestinger <jkoestinger@gmail.com>
2016-06-06 16:17:16 +0200

    Fixed references

--- src/derpina/Main.java
+++ src/derpina/Main.java
@@ -1,7 +1,22 @@
 package derpina;
 
-public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello World !");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/derpina/HomeScreen.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
