660d489f8492a7b3929180a7ea16e96252598b5e
jkoestinger <jkoestinger@gmail.com>
2016-06-20 11:04:05 +0200

    Added missing images

---
+++ src/derpina/ImageFinder.java
@@ -0,0 +1,36 @@
+package derpina;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ImageFinder {
+
+    private static String next;
+    private static String baseUrl;
+
+    public static void setBaseUrl(String url){
+        baseUrl = url;
+    }
+
+    public static void getNext(){
+        try {
+            URL url = new URL(baseUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+//                result.append(line);
+            }
+            reader.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
